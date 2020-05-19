73b1443897ae83273f34ecdf9a1b096d70b74065
joe <bazargaliyev.zhandos@gmail.com>
2020-05-19 17:24:31 +0600

    some check

--- src/views/CV/Section_05.vue
+++ src/views/CV/Section_05.vue
@@ -1,14 +1,33 @@
 <template>
   <div class="section-box s5-wrap" id="section5">
-    <h1>
+    <h1 @click="goto">
       Coming soon...
     </h1>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'Section_05'
+  name: 'Section_05',
+  methods: {
+    goto () {
+      //this.$router.push('/images')
+      axios
+        .post('https://www.kompra.kz/open-company/_search?keyword=apple%20city&size=20'
+        )
+        .then(response => {
+          console.log(response)
+        })
+        .catch(error => console.log(error))
+    }
+  }
+  // API.sendPhoto(imageNotDone).then((response) => {
+  //   if (response && response.success) {
+  //     responseImageNotDone = response.data
+  //   }
+  // }
 }
 </script>
 
